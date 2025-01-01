c1a1a878b2299106362ffbe3b5b1f16d05ef1209
Eduardo Almeida <edu.almeida1@gmail.com>
2025-01-01 20:41:07 +0000

    Fix layout for the article page

--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-article',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './article.component.html',
   styleUrls: ['../styles/articles-feature.scss', './article.component.scss']
 })
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  imports: [FontAwesomeModule]
 })
 export class FooterComponent implements OnInit {
+  faCoffee = faCoffee;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
